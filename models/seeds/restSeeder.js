f9053821eff36aa340da96027a745150eded8f6f
chuchushooes <chuchushooes@gmail.com>
2022-10-11 10:31:23 +0800

    feat: add restaurant seeder

--- models/seeds/restSeeder.js
+++ models/seeds/restSeeder.js
@@ -1,7 +1,10 @@
 // 導入 mongoose
 const mongoose = require('mongoose')
 mongoose.connect(`${process.env.MONGODB_URI_restaurant}`)
-const restaurantList = require("./restaurant.json") //導入restaurant json
+const restaurant = require('../restaurants') //導入restaurant js
+const restaurantList =  require('../../restaurant.json') // 上二層的路徑
+const stores = restaurantList.results
+// console.log(stores[1].name)
 
 // db設置
 const db = mongoose.connection
@@ -12,4 +15,23 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('mongodb connected!')
+  for(let i = 0; i < 8; i++) {
+
+    // restaurant.create(stores) 更快它會自己對應
+    restaurant.create( 
+      {
+       name: `${stores[i].name}`,
+       name_en: `${stores[i].name_en}`,
+       category: `${stores[i].category}`,
+       image: `${stores[i].image}`,
+       location: `${stores[i].location}`,
+       phone: `${stores[i].phone}`,
+       google_map: `${stores[i].google_map}`,
+       embed_map: `${stores[i].embed_map}`,
+       rating: `${stores[i].rating}`,
+       description: `${stores[i].description}`
+      },
+    )
+  }
+  console.log(`done.`)
 })
