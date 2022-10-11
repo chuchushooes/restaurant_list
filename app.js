2f76b975f7def0dafb64a24f6d97f10303df5c67
chuchushooes <chuchushooes@gmail.com>
2022-10-12 00:57:24 +0800

    feat: add update function

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// show store data, use params
+// show store data page, use params
 app.get('/restaurants/:id', (req, res) => {
   const id  = req.params.id
   // console.log(id)
@@ -55,8 +55,46 @@ app.get('/restaurants/:id', (req, res) => {
   .then(store => res.render('show',{ store }))
   .catch(error => console.log(error))
 })
-  
 
+
+// show edit store page, use params
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id  = req.params.id
+  return Restaurant.findById(id) // 找到id後回傳資料
+  .lean()
+  .then(store => res.render('edit',{ store }))
+  .catch(error => console.log(error))
+})
+
+
+// show add new store page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+
+// update store data
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id  = req.params.id
+  const body = req.body
+  //  return Restaurant.findByIdAndUpdate(id, req.body) 精簡用法
+  return Restaurant.findById(id)
+  .then(store => {
+    store.name = body.name
+    store.name_en = body.name_en
+    store.category = body.category
+    store.image = body.image
+    store.location = body.location
+    store.phone = body.phone
+    store.google_map = body.google_map
+    store.embed_map = body.embed_map
+    store.rating = body.rating
+    store.description = body.description
+    return store.save()
+  })
+  .then(() => res.redirect(`/restaurants/${id}`))
+  .catch(error => console.log(error))
+})
 
 // show Query String, use query
 app.get('/search', (req, res) => {
